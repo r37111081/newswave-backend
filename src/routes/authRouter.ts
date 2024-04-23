6584a9a8582958f1dcd540c6f56dcb09980b62e8
chrisT <r37111081@.github.com>
2024-04-23 20:54:52 +0800

    add swagger

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -7,8 +7,71 @@ import {
 
 const router = express.Router()
 
-router.post('/register', registerUser)
-router.post('/login', authenticateUser)
-router.post('/logout', logoutUser)
+router.post('/register',
+/**
+     * #swagger.tags = ['Users']
+     * #swagger.description  = "新使用者註冊"
+     * #swagger.parameters['body'] = {
+            in: 'body',
+            schema: {},
+        }
+     * #swagger.responses[200] = {
+            schema: {
+                "status": true,
+                "result": {},
+            }
+        }
+     * #swagger.responses[404] = {
+            schema: {
+                "status": false,
+                "message": "此最新消息不存在",
+            }
+        }
+     */
+  registerUser)
+router.post('/login',
+/**
+     * #swagger.tags = ['Users']
+     * #swagger.description  = "使用者登入"
+     * #swagger.parameters['body'] = {
+            in: 'body',
+            schema: {},
+        }
+     * #swagger.responses[200] = {
+            schema: {
+                "status": true,
+                "result": {},
+            }
+        }
+     * #swagger.responses[404] = {
+            schema: {
+                "status": false,
+                "message": "此最新消息不存在",
+            }
+        }
+     */
+  authenticateUser)
+router.post('/logout',
+/**
+     * #swagger.tags = ['Users']
+     * #swagger.description  = "使用者登出"
+     * #swagger.parameters['body'] = {
+            in: 'body',
+            schema: {},
+        }
+     * #swagger.responses[200] = {
+            schema: {
+                "status": true,
+                "result": {},
+            }
+        }
+     * #swagger.responses[404] = {
+            schema: {
+                "status": false,
+                "message": "此最新消息不存在",
+            }
+        }
+     */
+  logoutUser)
 
 export default router
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+  tags: [ // by default: empty Array
+    {
+      name: 'Users',
+      description: '使用者註冊登入'
+    }
+  ]
+}
+
+const outputFile = './swagger_output.json' // 輸出的文件名稱
+const endpointsFiles = ['./src/index.ts'] // 要指向的 API，通常使用 Express 直接指向到 app.js 就可以
+
+swaggerAutogen(outputFile, endpointsFiles, doc) // swaggerAutogen 的方法
