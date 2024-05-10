bd534d5e8806701ce0f143b1772c032dd42d95b6
sihyun-user <chsiyu8652@gmail.com>
2024-05-11 02:51:16 +0800

    feat: 新增取得雜誌文章列表功能

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import User from '../models/User'
 import Magazine from '../models/Magazine'
+import News from '../models/News'
 import { generateToken, clearToken } from '../utils/auth'
 
 const registerUser = async (req: Request, res: Response) => {
@@ -72,4 +73,43 @@ const getAllMagazine = async (req: Request, res: Response) => {
   }
 }
 
-export { registerUser, authenticateUser, logoutUser, getAllMagazine }
+const getMagazineList = async (req: Request, res: Response) => {
+  try {
+    const query = req.query
+    const articlesPerPage = 6
+
+    const category = query.category !== undefined ? { 'source.name': query.category } : {}
+    const pageIndex = query.pageIndex !== undefined && query.pageIndex !== ''
+      ? parseInt(query.pageIndex as string)
+      : 1
+
+    const [totalElements, articles] = await Promise.all([
+      News.countDocuments({ ...category }),
+      News.find({ ...category })
+        .skip((pageIndex - 1) * articlesPerPage)
+        .limit(articlesPerPage)
+    ])
+
+    const firstPage = pageIndex === 1
+    const lastPage = totalElements <= pageIndex * articlesPerPage
+    const empty = totalElements === 0
+    const totalPages = Math.ceil(totalElements / articlesPerPage)
+    res.status(200).json({
+      status: true,
+      message: '取得雜誌文章列表成功',
+      data: {
+        articles,
+        firstPage,
+        lastPage,
+        empty,
+        totalElements,
+        totalPages,
+        targetPage: pageIndex
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { registerUser, authenticateUser, logoutUser, getAllMagazine, getMagazineList }
--- swagger.js
+++ swagger.js
@@ -8,7 +8,34 @@ const doc = {
     }
   ],
   schemes: ['http', 'https'],
-  host: 'newswave-backend.onrender.com'
+  host: 'newswave-backend.onrender.com',
+  definitions: {
+    magazineInfo: {
+      status: true,
+      message: '取得雜誌文章列表成功',
+      data: {
+        articles: [
+          {
+            articleId: '文章ID',
+            title: '文章標題',
+            tags: '文章標籤',
+            publishedAt: '文章時間',
+            image: '文章圖片',
+            source: {
+              name: '文章來源名稱',
+              url: '文章來源網址'
+            }
+          }
+        ],
+        firstPage: '是否為第一頁',
+        lastPage: '是否為最後頁',
+        empty: '是否沒有資料',
+        totalElements: '總共有幾筆資料',
+        totalPages: '總共有幾頁',
+        targetPage: '目前在第幾頁'
+      }
+    }
+  }
 }
 
 const outputFile = './swagger_output.json' // 輸出的文件名稱
