0a35b1b1e7b297d5a12d9f7d5f94693ad9e78c8e
sihyun-user <chsiyu8652@gmail.com>
2024-05-11 09:41:01 +0800

    feat: 新增取得雜誌文章列表功能

---
+++ src/models/News.ts
@@ -0,0 +1,72 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+export interface INews extends Document {
+  articleId: string;
+  topic: string[];
+  editor: string;
+  title: string;
+  publishedAt: string;
+  imageDescribe: string;
+  image: string;
+  content: string;
+  source: {
+    name: string;
+    url: string;
+  };
+}
+
+const newsSchema = new Schema<INews>(
+  {
+    articleId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    topic: {
+      type: [String],
+      required: true
+    },
+    editor: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    publishedAt: {
+      type: String,
+      required: true
+    },
+    imageDescribe: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    source: {
+      name: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        default: '',
+        required: true
+      }
+    }
+  },
+  {
+    versionKey: false
+  }
+)
+
+const News = mongoose.model('New', newsSchema)
+
+export default News
