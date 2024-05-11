c89c93b01c55a7a908c032f34e9dcbfb949344e1
sihyun-user <chsiyu8652@gmail.com>
2024-05-11 09:41:01 +0800

    feat: 新增取得雜誌文章列表功能

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import User from '../models/User'
 import bcrypt from 'bcryptjs'
 import validator from 'validator'
+import News from '../models/News'
 import { catchAsync } from '../utils/catchAsync'
 import { appSuccess } from '../utils/appSuccess'
 import { appError } from '../middleware/errorMiddleware'
@@ -83,4 +84,45 @@ const getUserInfo = catchAsync(async (req: Request, res: Response, next: NextFun
     data: user
   })
 })
-export { getUser, updatePassword, getUserInfo }
+
+// 取得雜誌文章列表
+const getMagazineList = async (req: Request, res: Response) => {
+  try {
+    const query = req.query
+    const articlesPerPage = 6
+
+    const category = query.category !== undefined ? { 'source.name': query.category } : {}
+    const pageIndex = query.pageIndex !== undefined && query.pageIndex !== ''
+      ? parseInt(query.pageIndex as string)
+      : 1
+
+    const [totalElements, articles] = await Promise.all([
+      News.countDocuments({ ...category }),
+      News.find({ ...category })
+        .skip((pageIndex - 1) * articlesPerPage)
+        .limit(articlesPerPage)
+    ])
+
+    const firstPage = pageIndex === 1
+    const lastPage = totalElements <= pageIndex * articlesPerPage
+    const empty = totalElements === 0
+    const totalPages = Math.ceil(totalElements / articlesPerPage)
+    res.status(200).json({
+      status: true,
+      message: '取得雜誌文章列表成功',
+      data: {
+        articles,
+        firstPage,
+        lastPage,
+        empty,
+        totalElements,
+        totalPages,
+        targetPage: pageIndex
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { getUser, updatePassword, getUserInfo, getMagazineList }
