fd4ec42b14f83440a55dde0faa3dd8b235fc2b22
chrisT <r37111081@.github.com>
2024-05-14 14:32:39 +0800

    chore(render):調整router註釋內容

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,8 +7,6 @@ import { appError } from '../middleware/errorMiddleware'
 
 const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body
-  const userExists = await User.findOne({ email })
-
   if (email) {
     if (!validator.isEmail(email)) {
       return appError({ statusCode: 400, message: 'Email 格式不正確' }, next)
@@ -29,54 +27,39 @@ const registerUser = async (req: Request, res: Response, next: NextFunction) => 
   if (!name) {
     return appError({ statusCode: 400, message: '請輸入暱稱' }, next)
   }
+  const userExists = await User.findOne({ email })
 
   if (userExists) {
-    res.status(400).json({
-      status: false,
-      message: '使用者已存在'
-    })
-    return
+    return res.status(400).json({ status: false, message: '使用者已存在' })
   }
 
-  const user = await User.create({
-    name, email, password
-  })
-  if (user) {
-    const token = generateToken(res, user._id)
-    res.status(201).json({
-      status: true,
-      message: '註冊成功',
-      data: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        token
-      }
-    })
-  } else {
-    res.status(400).json({ status: false, message: '建立使用者發生錯誤' })
+  const user = await User.create({ name, email, password })
+
+  if (!user) {
+    return res.status(400).json({ status: false, message: '建立使用者發生錯誤' })
   }
+  const token = generateToken(res, user._id)
+
+  res.status(201).json({
+    status: true,
+    message: '註冊成功',
+    data: { id: user._id, name: user.name, email: user.email, token }
+  })
 }
 
 const authenticateUser = async (req: Request, res: Response) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
 
-  if (user && (await user.comparePassword(password))) {
-    const token = generateToken(res, user._id)
-    res.status(200).json({
-      status: true,
-      message: '登入成功',
-      data: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        token
-      }
-    })
-  } else {
-    res.status(401).json({ status: false, message: '找不到使用者 / 密碼錯誤' })
+  if (!user || !(await user.comparePassword(password))) {
+    return res.status(401).json({ status: false, message: '找不到使用者 / 密碼錯誤' })
   }
+  const token = generateToken(res, user._id)
+  res.status(200).json({
+    status: true,
+    message: '登入成功',
+    data: { id: user._id, name: user.name, email: user.email, token }
+  })
 }
 
 const logoutUser = (req: Request, res: Response) => {
@@ -87,14 +70,13 @@ const logoutUser = (req: Request, res: Response) => {
 const getAllMagazine = async (req: Request, res: Response) => {
   try {
     const data = await Magazine.find()
-
     res.status(200).json({
       status: true,
       message: '取得雜誌列表成功',
       data
     })
   } catch (error) {
-    console.error(error)
+    res.status(500).json({ status: false, message: '伺服器錯誤' })
   }
 }
 
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,105 +1,109 @@
 import express from 'express'
-import { registerUser, authenticateUser, logoutUser, getAllMagazine } from '../controllers/authController'
+import {
+  registerUser,
+  authenticateUser,
+  logoutUser,
+  getAllMagazine
+} from '../controllers/authController'
 
 const router = express.Router()
 
-router.post('/register',
-/**
-     * #swagger.tags = ['Users']
-     * #swagger.parameters['obj'] = {
-            in: 'body',
-            required: true,
-            description: "使用者註冊",
-            schema: {
-                "name":"user",
-                "email":"user@gmail.com",
-                "password":"user1234"
-            },
-        }
-     * #swagger.responses[201] = {
-            description: '註冊成功',
-            schema: {
-                "status": true,
-                "message": "註冊成功",
-                "uid": "QZDQO0zh...."
-            }
-        }
-     * #swagger.responses[400] = {
-            description: '使用者已存在',
-            schema: {
-                "status": false,
-                "message": "使用者已存在",
-            }
-        }
-     */
-  registerUser)
-router.post('/login',
-/**
-     * #swagger.tags = ['Users']
-     * #swagger.description  = "使用者登入"
-     * #swagger.parameters['body'] = {
-            in: 'body',
-            schema: {
-                "email":"user@gmail.com",
-                "password":"user1234"
-            },
-        }
-     * #swagger.responses[200] = {
-            schema: {
-                "status": true,
-                "result": {
-                    "status": true,
-                    "message": "登入成功",
-                    "data": {
-                        "id": "6640aacaad9a2f2b....",
-                        "name": "user",
-                        "email": "user@gmail.com",
-                        "token": "eyJhbGci......"
-                    }
+router.post(
+  '/register',
+  /**
+         * #swagger.tags = ['Users']
+         * #swagger.parameters['obj'] = {
+                in: 'body',
+                required: true,
+                description: "使用者註冊",
+                schema: {
+                    "name":"user",
+                    "email":"user@gmail.com",
+                    "password":"user1234"
                 },
             }
-        }
-     * #swagger.responses[404] = {
-            schema: {
-                "status": false,
-                "message": "此最新消息不存在",
+         * #swagger.responses[201] = {
+                description: '註冊成功',
+                schema: {
+                    "status": true,
+                    "message": "註冊成功",
+                    "uid": "QZDQO0zh...."
+                }
             }
-        }
-     */
-  authenticateUser)
-router.post('/logout',
-/**
-     * #swagger.tags = ['Users']
-     * #swagger.ignore = true
-     * #swagger.description  = "使用者登出"
-     * #swagger.parameters['body'] = {
-            in: 'body',
-            schema: {},
-        }
-     * #swagger.responses[200] = {
-            schema: {
-                "status": true,
-                "result": {},
+         * #swagger.responses[400] = {
+                description: '使用者已存在',
+                schema: {
+                    "status": false,
+                    "message": "使用者已存在",
+                }
             }
-        }
-     * #swagger.responses[404] = {
-            schema: {
-                "status": false,
-                "message": "此最新消息不存在",
+         */
+  registerUser
+)
+router.post(
+  '/login',
+  /**
+         * #swagger.tags = ['Users']
+         * #swagger.description  = "使用者登入"
+         * #swagger.parameters['body'] = {
+                in: 'body',
+                schema: {
+                    "email":"user@gmail.com",
+                    "password":"user1234"
+                },
             }
-        }
-     */
-  logoutUser)
-router.get('/magazine-category-list',
-/**
-    #swagger.tags = ['Magazine']
-    #swagger.description  = "取得雜誌種類列表"
+         * #swagger.responses[200] = {
+                schema: {
+                    "status": true,
+                    "result": {},
+                }
+            }
+         * #swagger.responses[404] = {
+                schema: {
+                    "status": false,
+                    "message": "此最新消息不存在",
+                }
+            }
+         */
+  authenticateUser
+)
+router.post(
+  '/logout',
+  /**
+         * #swagger.tags = ['Users']
+         * #swagger.ignore = true
+         * #swagger.description  = "使用者登出"
+         * #swagger.parameters['body'] = {
+                in: 'body',
+                schema: {},
+            }
+         * #swagger.responses[200] = {
+                schema: {
+                    "status": true,
+                    "result": {},
+                }
+            }
+         * #swagger.responses[404] = {
+                schema: {
+                    "status": false,
+                    "message": "此最新消息不存在",
+                }
+            }
+         */
+  logoutUser
+)
+router.get(
+  '/magazine-category-list',
+  /**
+        #swagger.tags = ['Magazine']
+        #swagger.description  = "取得雜誌種類列表"
 
-    #swagger.responses[200] = {
-        description: '雜誌種類列表資訊',
-        schema: { $ref: '#/definitions/magazineList' }
-    }
-*/
-  getAllMagazine)
+        #swagger.responses[200] = {
+            description: '雜誌種類列表資訊',
+            schema: { $ref: '#/definitions/magazineList' }
+        }
+    */
+  getAllMagazine
+)
 
 export default router
